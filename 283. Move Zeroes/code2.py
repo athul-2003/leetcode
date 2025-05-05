a29585a89ffec6b80ff906fba190b5afd1bb548c
H ATHULKRISHNAN <128019369+athul-2003@users.noreply.github.com>
2025-05-05 08:54:12 +0530

    Create code2.py

---
+++ 283. Move Zeroes/code2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        res=0
+        n=len(nums)
+
+        for i in range(n):
+            if nums[i]!=0:
+                nums[i],nums[res]=nums[res],nums[i]
+                res+=1
